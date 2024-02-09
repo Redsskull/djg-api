0620e4848358420d72f9b63d00770e5f41be790c
Redsskull <ivry91@gmail.com>
2024-02-09 10:13:18 +0100

    add filtering

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
 from .models import Post
+from  likes.models import Like
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
+    comment_count = serializers.ReadOnlyField()
+    like_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -18,6 +22,13 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context.get('request')
         return request.user == obj.owner
     
+    def get_like_id(self, obj):
+        request = self.context.get('request')
+        if request.user.is_authenticated:
+            like = Like.objects.filter(owner=request.user, post=obj).first()
+            return like.id if like else None
+        return None
+    
     class Meta:
         model = Post
-        fields = ['id', 'owner', 'title', 'content', 'image', 'created_at', 'updated_at', 'is_owner', 'profile_id', 'profile_image', 'image_filter']
+        fields = ['id', 'owner', 'title', 'content', 'image', 'created_at', 'updated_at', 'is_owner', 'profile_id', 'profile_image', 'image_filter', 'like_id', 'comment_count', 'like_count']
--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,9 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
-
-
+from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
 class PostList(generics.ListCreateAPIView):
     """
     List posts or create a post if logged in
@@ -11,7 +11,16 @@ class PostList(generics.ListCreateAPIView):
     """
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+      comments_count=Count('comment', distinct=True),
+      like_count=Count('likes', distinct=True)
+    ).order_by('-created_at')
+    filter_backends = [filters.OrderingFilter,
+                       filters.SearchFilter,
+                       DjangoFilterBackend]
+    filterset_fields = ['owner__followed__owner__profile', 'likes__owner__profile', 'owner__profile']
+    search_fields = ['title', 'owner__username']
+    ordering_fields = ['comment_count', 'like_count', 'created_at']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
