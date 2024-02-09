c05519e69236709658e05a1c3f37dcca8e3b1658
Redsskull <ivry91@gmail.com>
2024-02-09 08:17:40 +0100

    add integrity error

---
+++ likes/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Like
+from django.db import IntegrityError
+
+class LikeSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Like
+        fields = ['id', 'owner', 'post', 'created_at']
+    
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except integrityError:
+            raise serializers.ValidationError({
+                'detail': 'You have already liked this post.'
+            })
