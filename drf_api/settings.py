f4ef700ef38a1dce26878d8a036e3434db07a742
Redsskull <ivry91@gmail.com>
2024-02-11 20:59:10 +0100

    fixed cors oprigin

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -103,14 +103,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-         os.environ.get('CLIENT_ORIGIN')
-    ]
-else:
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-         r"^https://.*\.gitpod\.io$",
-    ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 
 CORS_ALLOW_CREDENTIALS = True    
 
